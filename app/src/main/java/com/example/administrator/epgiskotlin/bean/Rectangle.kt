038cfd699b314af9846b21a0a77b2dae504de45f
1296695625 <1296695625@qq.com>
2020-07-01 17:40:19 +0800

    ....

---
+++ app/src/main/java/com/example/administrator/epgiskotlin/bean/Rectangle.kt
@@ -0,0 +1,13 @@
+package com.example.administrator.epgiskotlin.bean
+
+/**
+ * Created by liu on 2020/6/3.
+ */
+class Rectangle(round: Double) : Shape(round) {
+
+    fun length() {}
+
+    override fun radius() {
+        super.radius()
+    }
+}
